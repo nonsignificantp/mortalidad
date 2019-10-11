8e34ea26c16ac6dff0971880bae3345cc36de590
Agustin Rodriguez <gus990@gmail.com>
2019-10-11 11:51:56 -0300

    transform historial to cvrisk

--- src/features/cvrisk.R
+++ src/features/cvrisk.R
@@ -1,18 +1,26 @@
 library('tidyverse')
 
+# Set connection to database --------------------------------------------------
+
+connection <- DBI::dbConnect(
+  RPostgres::Postgres(), 
+  host = "database",
+  user = "postgres",
+  dbname = 'mortalidad',
+  password = "postgres"
+)
+
 # Load data -------------------------------------------------------------------
 
-input_historial_csv <- file.path('data', 'stable', 'historial.csv')
-historial <- read_csv(input_historial_csv)
+historial <- tbl(connection, 'historial')
 
 # Featuring -------------------------------------------------------------------
 
-cvrisk <- historial %>% 
+query <- historial %>% 
   mutate(hta = ifelse(tas >= 140 | tad >= 90, yes=1, no=0),
-         cv = rowSums(select(., iam, acv, eap, fa, icc))) %>% 
+         cv = iam + acv + eap + fa + icc) %>% 
   select(paciente_id, hta, cv, dbt)
 
 # Save ------------------------------------------------------------------------
   
-output_cvrisk_cvs <- file.path('data', 'features', 'cvrisk.csv')
-write_csv(cvrisk, output_cvrisk_cvs)
+copy_to(connection, query, 'cvrisk', overwrite = T, temporary = F)
